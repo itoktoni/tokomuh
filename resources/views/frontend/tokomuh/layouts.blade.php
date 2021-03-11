198752083c77c9f7046c332363a28fc5d2f98f42
itok toni laksono <itok.toni@gmail.com>
2021-03-11 10:54:49 +0700

    update wa on bottom

--- resources/views/frontend/tokomuh/layouts.blade.php
+++ resources/views/frontend/tokomuh/layouts.blade.php
@@ -3,9 +3,9 @@
 
 <head>
 
-@include(Helper::setExtendFrontend('meta'))
-@include(Helper::setExtendFrontend('css'))
-@livewireStyles
+    @include(Helper::setExtendFrontend('meta'))
+    @include(Helper::setExtendFrontend('css'))
+    @livewireStyles
 
 </head>
 
@@ -30,10 +30,14 @@
     <!-- Scroll Top -->
     <a id="scroll-top" href="#top" title="Top" role="button" class="scroll-top"><i class="fas fa-chevron-up"></i></a>
 
+    <a class="wa" target="_blank" href="https://api.whatsapp.com/send?phone={{ Helper::convertPhone(config('website.phone')) }}">
+        <img class="wa" src="{{ Helper::files('logo/wa.png') }}" alt="wa">
+    </a>
+
     @include(Helper::setExtendFrontend('mobile_menu'))
     @include(Helper::setExtendFrontend('js'))
     @livewireScripts
-    
+
 </body>
 
 </html>
